36934e74fa046ad75d452cd713f3dab13a80e560
Ryu-Dev-Here <ryureaper133@gmail.com>
2025-12-28 22:27:48 +0530

    typo fix

--- Server/next.config.js
+++ Server/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'standalone',
+
   typescript: {
     ignoreBuildErrors: false,
   },
+
   async headers() {
     return [
       {
@@ -11,12 +15,19 @@ const nextConfig = {
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, X-HotShield-Signature, X-HotShield-Timestamp' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, X-HotShield-Signature, X-HotShield-Timestamp',
+          },
         ],
       },
     ];
   },
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
-
